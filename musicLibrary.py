75413f914cf6643516846bf2049692672389e5cf
MdAhsan07 <mdahsan7904@gmail.com>
2025-09-22 10:36:27 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "despacito": "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "enchanted": "https://www.youtube.com/watch?v=igIfiqqVHtA&pp=ygUJZW5jaGFudGVk",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
+}
